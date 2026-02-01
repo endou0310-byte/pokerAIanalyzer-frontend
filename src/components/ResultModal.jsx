b252dd376e499bb350cfafd0405bdc4e00fb1f8e
endou0310-byte <endou0310@gmail.com>
2026-02-01 22:50:14 +0900

    feat: update source files

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -512,10 +512,11 @@ export default function ResultModal({
                         alignItems: "center",
                         justifyContent: "center",
                         cursor: "pointer",
-                        padding: 0
+                        padding: 0,
+                        overflow: "hidden"
                       }}
                     >
-                      <svg viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></svg>
+                      <img src="/img/logo.svg" alt="X" style={{ width: "30px", height: "30px", objectFit: "contain" }} />
                     </button>
                     <span style={{ fontSize: 12, color: "#aaa" }}>X</span>
                   </div>
@@ -528,17 +529,17 @@ export default function ResultModal({
                         width: 60,
                         height: 60,
                         borderRadius: "50%",
-                        background: "#06c755", // LINE Green
+                        background: "#fff", // White bg for colored icon
                         border: "none",
-                        color: "#fff",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
                         cursor: "pointer",
-                        padding: 0
+                        padding: 0,
+                        overflow: "hidden"
                       }}
                     >
-                      <svg viewBox="0 0 24 24" width="30" height="30" fill="currentColor"><path d="M22 10.8c0-4.8-5.1-8.8-11.5-8.8S-1 6-1 10.8c0 4.1 3.5 7.6 8.4 8.5.6.1 1.4.3 1.1 1.1-.1.4-.4 1.5-.5 1.8-.3 1.2-1.4 2.7 1.2 1.4 5.3-2.6 11.3-6.9 11.3-12.8zM6.9 13.1H5V8.5h1.9v4.6zm3.3 0H8.3V8.5h1.9v4.6zm3.3 0h-1.9V8.5h1.7l1.3 2.1V8.5h1.9v4.6h-1.7l-1.3-2.1v2.1zm4.6 0v-4.6h3.4v1.6H20v1.4h1.5v3.2-.2z"></path></svg>
+                      <img src="/img/icons8-line-96.svg" alt="LINE" style={{ width: "60px", height: "60px", objectFit: "cover" }} />
                     </button>
                     <span style={{ fontSize: 12, color: "#aaa" }}>LINE</span>
                   </div>
@@ -551,17 +552,17 @@ export default function ResultModal({
                         width: 60,
                         height: 60,
                         borderRadius: "50%",
-                        background: "#5865F2", // Discord Blurple
+                        background: "#fff", // White bg for colored icon
                         border: "none",
-                        color: "#fff",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
                         cursor: "pointer",
-                        padding: 0
+                        padding: 0,
+                        overflow: "hidden"
                       }}
                     >
-                      <svg viewBox="0 0 24 24" width="30" height="30" fill="currentColor"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.086 2.157 2.419 0 1.334-.956 2.419-2.157 2.419zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.086 2.157 2.419 0 1.334-.946 2.419-2.157 2.419z"></path></svg>
+                      <img src="/img/icons8-discordの新しいロゴ-96.svg" alt="Discord" style={{ width: "60px", height: "60px", objectFit: "cover" }} />
                     </button>
                     <span style={{ fontSize: 12, color: "#aaa" }}>Discord</span>
                   </div>
@@ -574,17 +575,17 @@ export default function ResultModal({
                         width: 60,
                         height: 60,
                         borderRadius: "50%",
-                        background: "#1877F2", // Facebook Blue
+                        background: "#fff", // White bg for colored icon
                         border: "none",
-                        color: "#fff",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
                         cursor: "pointer",
-                        padding: 0
+                        padding: 0,
+                        overflow: "hidden"
                       }}
                     >
-                      <svg viewBox="0 0 24 24" width="30" height="30" fill="currentColor"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path></svg>
+                      <img src="/img/2021_Facebook_icon.svg" alt="Facebook" style={{ width: "60px", height: "60px", objectFit: "cover" }} />
                     </button>
                     <span style={{ fontSize: 12, color: "#aaa" }}>Facebook</span>
                   </div>
